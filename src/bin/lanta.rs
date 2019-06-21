e1bba7a6cb9319fdf97f87cc73c3e3d50e9493fc
Jimmy Brisson <theotherjimmy@gmail.com>
2019-06-21 14:26:56 -0500

    Use new features in the config

--- src/bin/lanta.rs
+++ src/bin/lanta.rs
@@ -24,30 +24,35 @@ fn main() -> Result<()> {
     #[rustfmt::skip]
     let mut keys = keys![
         ([modkey], XK_d, cmd::lazy::close_focused_window()),
-        ([modkey], XK_j, cmd::lazy::focus_next()),
-        ([modkey], XK_k, cmd::lazy::focus_previous()),
-        ([modkey, shift], XK_j, cmd::lazy::shuffle_next()),
-        ([modkey, shift], XK_k, cmd::lazy::shuffle_previous()),
+        ([modkey], XK_Right, cmd::lazy::focus_next()),
+        ([modkey], XK_Left, cmd::lazy::focus_previous()),
+        ([modkey], XK_r, cmd::lazy::next_group()),
+        ([modkey], XK_v, cmd::lazy::prev_group()),
+        ([modkey, shift], XK_Right, cmd::lazy::shuffle_next()),
+        ([modkey, shift], XK_Left, cmd::lazy::shuffle_previous()),
         ([modkey], XK_Tab, cmd::lazy::layout_next()),
 
         ([modkey], XK_c, spawn!("alacritty")),
         ([modkey], XK_p, spawn!("dmenu_run")),
+
+        ([modkey, shift], XK_a, spawn!("amixer", "-q", "set", "Master", "2%+")),
+        ([modkey], XK_a, spawn!("amixer", "-q", "set", "Master", "2%-")),
     ];
 
-    let padding = 0;
+    let padding = 2;
     let layouts = layouts![
         StackLayout::new("stack", 0),
         TiledLayout::new("tiled", padding),
+        ThreeColumn::new("3 column", padding),
     ];
 
     let groups = groups! {
         keys,
         shift,
         [
-            ([modkey], XK_a, "chrome", "stack"),
-            ([modkey], XK_s, "code", "stack"),
-            ([modkey], XK_d, "term", "tiled"),
-            ([modkey], XK_f, "misc", "tiled"),
+            ([modkey, shift], XK_1, "browser", "stack"),
+            ([modkey, shift], XK_3, "term", "3 column"),
+            ([modkey, shift], XK_5, "misc", "3 column"),
         ]
     };
 
