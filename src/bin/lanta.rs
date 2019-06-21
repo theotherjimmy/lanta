f58d861836f0bb703d2974bbf1e882dbeb857dc4
Jimmy Brisson <theotherjimmy@gmail.com>
2019-06-21 09:14:53 -0500

    Customize key bindings

--- src/bin/lanta.rs
+++ src/bin/lanta.rs
@@ -18,37 +18,24 @@ macro_rules! spawn {
 fn main() -> Result<()> {
     lanta::intiailize_logger()?;
 
-    let modkey = ModKey::Mod4;
+    let modkey = ModKey::Mod1;
     let shift = ModKey::Shift;
 
     #[rustfmt::skip]
     let mut keys = keys![
-        ([modkey], XK_w, cmd::lazy::close_focused_window()),
+        ([modkey], XK_d, cmd::lazy::close_focused_window()),
         ([modkey], XK_j, cmd::lazy::focus_next()),
         ([modkey], XK_k, cmd::lazy::focus_previous()),
         ([modkey, shift], XK_j, cmd::lazy::shuffle_next()),
         ([modkey, shift], XK_k, cmd::lazy::shuffle_previous()),
         ([modkey], XK_Tab, cmd::lazy::layout_next()),
 
-        ([modkey], XK_Return, spawn!("urxvt")),
-        ([modkey], XK_c, spawn!("firefox")),
-        ([modkey], XK_v, spawn!("code-oss")),
-        ([modkey], XK_b, spawn!("spotify --force-device-scale-factor=2")),
-        ([modkey], XK_q, spawn!("change-wallpaper")),
-
-        ([], XF86XK_MonBrightnessUp, spawn!("xbacklight", "-inc", "10")),
-        ([], XF86XK_MonBrightnessDown, spawn!("xbacklight", "-dec", "10")),
-        ([], XF86XK_AudioPrev, spawn!("playerctl", "previous")),
-        ([], XF86XK_AudioPlay, spawn!("playerctl", "play-pause")),
-        ([], XF86XK_AudioNext, spawn!("playerctl", "next")),
-        ([], XF86XK_AudioRaiseVolume, spawn!("amixer", "-q", "set", "Master", "5%+")),
-        ([], XF86XK_AudioLowerVolume, spawn!("amixer", "-q", "set", "Master", "5%-")),
-        ([], XF86XK_AudioMute, spawn!("amixer", "-q", "set", "Master", "toggle")),
+        ([modkey], XK_c, spawn!("alacritty")),
+        ([modkey], XK_p, spawn!("dmenu_run")),
     ];
 
-    let padding = 20;
+    let padding = 0;
     let layouts = layouts![
-        StackLayout::new("stack-padded", padding),
         StackLayout::new("stack", 0),
         TiledLayout::new("tiled", padding),
     ];
