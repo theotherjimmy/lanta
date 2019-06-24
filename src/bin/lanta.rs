a9c3609c669e1466dd5025871e5ad535fc05e93d
Jimmy Brisson <theotherjimmy@gmail.com>
2019-06-24 13:45:30 -0500

    Rewrite configuration

--- src/bin/lanta.rs
+++ src/bin/lanta.rs
@@ -1,8 +1,9 @@
-#[macro_use]
 extern crate lanta;
 
+use std::rc::Rc;
 use lanta::layout::*;
-use lanta::{cmd, Lanta, ModKey, Result};
+use lanta::keysym::*;
+use lanta::{cmd, Lanta, ModKey, Result, GroupBuilder};
 
 macro_rules! spawn {
     ($cmd:expr) => (::lanta::cmd::lazy::spawn(::std::process::Command::new($cmd)));
@@ -16,46 +17,40 @@ macro_rules! spawn {
 }
 
 fn main() -> Result<()> {
-    lanta::intiailize_logger()?;
-
+    let padding = 2;
     let modkey = ModKey::Mod1;
     let shift = ModKey::Shift;
 
-    #[rustfmt::skip]
-    let mut keys = keys![
-        ([modkey], XK_d, cmd::lazy::close_focused_window()),
-        ([modkey], XK_Right, cmd::lazy::focus_next()),
-        ([modkey], XK_Left, cmd::lazy::focus_previous()),
-        ([modkey], XK_r, cmd::lazy::next_group()),
-        ([modkey], XK_v, cmd::lazy::prev_group()),
-        ([modkey, shift], XK_Right, cmd::lazy::shuffle_next()),
-        ([modkey, shift], XK_Left, cmd::lazy::shuffle_previous()),
-        ([modkey], XK_Tab, cmd::lazy::layout_next()),
+    let keys = vec![
+        (vec![modkey], XK_d, cmd::lazy::close_focused_window()),
+        (vec![modkey], XK_l, cmd::lazy::focus_next()),
+        (vec![modkey, shift], XK_l, cmd::lazy::shuffle_next()),
+        (vec![modkey], XK_h, cmd::lazy::focus_previous()),
+        (vec![modkey, shift], XK_h, cmd::lazy::shuffle_previous()),
+        (vec![modkey], XK_Down, cmd::lazy::next_group()),
+        (vec![modkey, shift], XK_Down, cmd::lazy::move_window_to_next_group()),
+        (vec![modkey], XK_Up, cmd::lazy::prev_group()),
+        (vec![modkey, shift], XK_Up, cmd::lazy::move_window_to_prev_group()),
+        (vec![modkey], XK_Tab, cmd::lazy::layout_next()),
 
-        ([modkey], XK_c, spawn!("alacritty")),
-        ([modkey], XK_p, spawn!("dmenu_run")),
-
-        ([modkey, shift], XK_a, spawn!("amixer", "-q", "set", "Master", "2%+")),
-        ([modkey], XK_a, spawn!("amixer", "-q", "set", "Master", "2%-")),
+        (vec![modkey], XK_c, spawn!("alacritty")),
+        (vec![modkey], XK_p, spawn!("dmenu_run")),
+        (vec![modkey], XK_a, spawn!("amixer", "-q", "set", "Master", "2%-")),
+        (vec![modkey, shift], XK_a, spawn!("amixer", "-q", "set", "Master", "2%+")),
+        (vec![modkey], XK_q, Rc::new(|_| panic!("exiting")))
     ];
 
-    let padding = 2;
-    let layouts = layouts![
-        StackLayout::new("stack", 0),
-        TiledLayout::new("tiled", padding),
-        ThreeColumn::new("3 column", padding),
+    let layouts: Vec<Box<dyn Layout>> = vec![
+        Box::new(StackLayout::new("stack", 0)),
+        Box::new(ThreeColumn::new("3 column", padding)),
+    ];
+    let groups = vec![
+        GroupBuilder::new("browser", "stack"),
+        GroupBuilder::new("term", "3 column"),
+        GroupBuilder::new("misc", "3 column"),
     ];
 
-    let groups = groups! {
-        keys,
-        shift,
-        [
-            ([modkey, shift], XK_1, "browser", "stack"),
-            ([modkey, shift], XK_3, "term", "3 column"),
-            ([modkey, shift], XK_5, "misc", "3 column"),
-        ]
-    };
-
+    lanta::intiailize_logger()?;
     Lanta::new(keys, groups, &layouts)?.run();
 
     Ok(())
