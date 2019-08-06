22bd51e562ebfad2037e1b0bf57c54388796320b
Jimmy Brisson <theotherjimmy@gmail.com>
2019-08-06 18:09:12 -0500

    Improve name for Screen's inner member

--- src/lib.rs
+++ src/lib.rs
@@ -75,27 +75,27 @@ struct Dock {
 
 #[derive(Default)]
 struct Screen {
-    vec: Vec<Dock>,
+    docks: Vec<Dock>,
 }
 
 impl Screen {
     pub fn add_dock(&mut self, conn: &Connection, window_id: WindowId) {
         let strut_partial = conn.get_strut_partial(&window_id);
-        self.vec.push(Dock {
+        self.docks.push(Dock {
             window_id,
             strut_partial,
         });
     }
 
     pub fn remove_dock(&mut self, window_id: &WindowId) {
-        self.vec.retain(|d| &d.window_id != window_id);
+        self.docks.retain(|d| &d.window_id != window_id);
     }
 
     /// Figure out the usable area of the screen based on the STRUT_PARTIAL of
     /// all docks.
     pub fn viewport(&self, screen_width: u32, screen_height: u32) -> Viewport {
         let (left, right, top, bottom) = self
-            .vec
+            .docks
             .iter()
             .filter_map(|o| o.strut_partial.as_ref())
             .fold((0, 0, 0, 0), |(left, right, top, bottom), s| {
