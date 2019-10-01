d4417ff669b4de912262d55addeb5d592e32515d
Jimmy Brisson <theotherjimmy@gmail.com>
2019-10-01 14:27:43 -0500

    Draw 1/3 and 2/3 with 3 column layout
    When 2 windows are used for the 2 column layout, use the left
    third of the screen and the remaining screen for the windows.

--- src/layout/tiled.rs
+++ src/layout/tiled.rs
@@ -62,56 +62,73 @@ impl<T: Copy> Layout<T> for ThreeColumn {
     }
 
     fn layout(&self, viewport: &Viewport, stack: &Stack<T>) -> Vec<MappedWindow<T>> {
-        if stack.len() == 0 {
-            Default::default()
-        } else if stack.len() < 3 {
-            let tile_width = (viewport.width - (self.inner_padding * (stack.len() as u32 - 1))) / stack.len() as u32;
-
-            stack
-                .iter()
-                .enumerate()
-                .map(|(i, &id)| {
-                    let vp = Viewport {
-                        x: viewport.x + (i as u32 * (tile_width + self.inner_padding)),
+        match stack.len() {
+            0 => Default::default(),
+            1 => vec![MappedWindow {
+                id: stack.iter().next().unwrap().clone(),
+                vp: viewport.clone(),
+            }],
+            2 => {
+                let left_width = (viewport.width - self.inner_padding) / 3;
+                let right_width = 2 * left_width
+                    + (viewport.width - self.inner_padding)
+                        .checked_rem(3)
+                        .unwrap();
+                let viewports = vec![
+                    Viewport {
+                        x: viewport.x,
                         y: viewport.y,
-                        width: tile_width,
+                        width: left_width,
                         height: viewport.height,
-                    };
-                    MappedWindow { id, vp }
-                })
-                .collect()
-        } else {
-            let tile_width = (viewport.width - (self.inner_padding * 2)) / 3;
-            let win_per_col = stack.len() / 3;
-            let leftovers = stack.len() - 3 * win_per_col;
-            let cols = match leftovers {
-                2 => vec![
-                    stack.slice(0..win_per_col + 1),
-                    stack.slice(win_per_col + 1..(2 * win_per_col) + 1),
-                    stack.slice((2 * win_per_col) + 1..stack.len()),
-                ],
-                _ => vec![
-                    stack.slice(0..win_per_col),
-                    stack.slice(win_per_col..(2 * win_per_col) + leftovers),
-                    stack.slice((2 * win_per_col) + leftovers..stack.len()),
-                ],
-            };
-            let mut to_ret = Vec::with_capacity(stack.len());
-            for (col, slice) in cols.iter().enumerate() {
-                let x = viewport.x + (col as u32 * (tile_width + self.inner_padding));
-                let tile_height =
-                    (viewport.height - (self.inner_padding * (slice.len() as u32 - 1))) / slice.len() as u32;
-                for (row, &id) in slice.iter().enumerate() {
-                    let vp = Viewport {
-                        x,
-                        y: viewport.y + (row as u32 * (tile_height + self.inner_padding)),
-                        width: tile_width,
-                        height: tile_height,
-                    };
-                    to_ret.push(MappedWindow { id, vp });
-                }
+                    },
+                    Viewport {
+                        x: viewport.x + self.inner_padding + left_width,
+                        y: viewport.y,
+                        width: right_width,
+                        height: viewport.height,
+                    },
+                ];
+
+                stack
+                    .iter()
+                    .zip(viewports.into_iter())
+                    .map(|(&id, vp)| MappedWindow { id, vp })
+                    .collect()
             }
-            to_ret
+            _ => {
+                let tile_width = (viewport.width - (self.inner_padding * 2)) / 3;
+                let win_per_col = stack.len() / 3;
+                let leftovers = stack.len() - 3 * win_per_col;
+                let cols = match leftovers {
+                    2 => vec![
+                        stack.slice(0..win_per_col + 1),
+                        stack.slice(win_per_col + 1..(2 * win_per_col) + 1),
+                        stack.slice((2 * win_per_col) + 1..stack.len()),
+                    ],
+                    _ => vec![
+                        stack.slice(0..win_per_col),
+                        stack.slice(win_per_col..(2 * win_per_col) + leftovers),
+                        stack.slice((2 * win_per_col) + leftovers..stack.len()),
+                    ],
+                };
+                let mut to_ret = Vec::with_capacity(stack.len());
+                for (col, slice) in cols.iter().enumerate() {
+                    let x = viewport.x + (col as u32 * (tile_width + self.inner_padding));
+                    let tile_height = (viewport.height
+                        - (self.inner_padding * (slice.len() as u32 - 1)))
+                        / slice.len() as u32;
+                    for (row, &id) in slice.iter().enumerate() {
+                        let vp = Viewport {
+                            x,
+                            y: viewport.y + (row as u32 * (tile_height + self.inner_padding)),
+                            width: tile_width,
+                            height: tile_height,
+                        };
+                        to_ret.push(MappedWindow { id, vp });
+                    }
+                }
+                to_ret
+            }
         }
     }
 }
